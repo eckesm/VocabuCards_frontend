09b37a5252497c8b0c9e2f4d634ba3d3eed38f9c
Matthew Eckes <eckesm@gmail.com>
2021-08-05 11:08:46 -0400

    Fixed issue preventing Heroku build.

--- src/components/Routes/RestrictedRoute.js
+++ src/components/Routes/RestrictedRoute.js
@@ -8,21 +8,14 @@ import RestrictedRouteScreen from './RestrictedRouteScreen';
 import ProtectedRouteScreen from './ProtectedRouteScreen';
 
 export default function RestrictedRoute({ component: Component, path, ...rest }) {
-<<<<<<< HEAD
-	const { user, subscription_status } = useSelector(st => st);
-=======
 	const { user, subscription_status, account_override } = useSelector(st => st);
->>>>>>> dev
 
 	if (!user) {
 		return <Route {...rest} render={props => <ProtectedRouteScreen />} />;
 	}
-<<<<<<< HEAD
-=======
 	else if (account_override === 'full_access') {
 		return <Route {...rest} render={props => <Component {...props} {...rest} />} />;
 	}
->>>>>>> dev
 	else {
 		return (
 			<Route
